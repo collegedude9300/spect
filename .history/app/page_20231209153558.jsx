23c5a53b0921eb1d9133e446f1b8809f7514d56b
collegedude9300 <collegenm2012@gmail.com>
2023-12-09 15:36:19 -0800

    Changing the title.

--- .history/app/page_20231209153558.jsx
+++ .history/app/page_20231209153558.jsx
@@ -4,16 +4,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { VStack, Button } from '@chakra-ui/react'
 import styles from './page.module.css'
-const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
       <title>Spectrum Station School</title>
-      <h1>Zen School</h1>
-      <h2>Welcome to Spectrum Station School, the school for people with autism and related disabilities.</h2>
-      <VStack padding= "10">
-        <Button backgroundColor="brand.100"> Brand 100 Test </Button>  
-        </VStack>
+      <h1>Spectrum Station School</h1>
+      <h2>Welcome to Spectrum Station School, the school for people with Autism and related disabilities.</h2>
     </main>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <title>Spectrum Station School</title>
-      <h1>Zen School</h1>
+      <h1>Spectrum Station School</h1>
       <h2>Welcome to Spectrum Station School, the school for people with Autism and related disabilities.</h2>
     </main>
   )
